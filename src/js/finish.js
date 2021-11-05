8090a76a258c227af8d5ba3b032f28f86ad20df4
Artem Kulyhin <akulyhin@gmail.com>
2021-11-05 09:09:29 +0200

    Last updates

--- src/js/finish.js
+++ src/js/finish.js
@@ -1,8 +1,12 @@
+
+var message = '<center><p class="success"><b>Проект успешно отправлен на проверку! Спасибо!</b></p></center>';
+
 $(document).ready(function() {
     $('#project').submit(function() {
       $('button.form-submit').attr('disabled', 'disabled');
+      $('#project').hide('slow');
+      $('#post_form_success').html(message);
         $.post("https://calltrack.com.ua/projects/telegram.php", $("#project").serialize(),  function(response) {
-            $('#project').hide('slow');
             $('#post_form_success').html(response);
             $('button.form-submit').attr('disabled', false);
         });
