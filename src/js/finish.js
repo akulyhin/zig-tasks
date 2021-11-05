985ff0918e95ac01797e8c426ea7427d3f6d9df4
Artem Kulyhin <akulyhin@gmail.com>
2021-11-05 09:19:43 +0200

    Last updates

--- src/js/finish.js
+++ src/js/finish.js
@@ -1,9 +1,22 @@
+const textCheckbox = document.getElementById('text');
+
+textCheckbox.addEventListener('click', (e) => {
+  if (e.target.checked) {
+    document.querySelector('.form-submit').removeAttribute('disabled');
+  }
+
+  else {
+    document.querySelector('.form-submit').setAttribute('disabled', 'disabled');
+  }
+})
+
+
 
 var message = '<center><p class="success"><b>Проект успешно отправлен на проверку! Спасибо!</b></p></center>';
 
 $(document).ready(function() {
     $('#project').submit(function() {
-      $('button.form-submit').attr('disabled', 'disabled');
+      $('.form-submit').attr('disabled', 'disabled');
       $('#project').hide('slow');
       $('#post_form_success').html(message);
         $.post("https://calltrack.com.ua/projects/telegram.php", $("#project").serialize(),  function(response) {
